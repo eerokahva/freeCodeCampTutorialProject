8f18ae57d1be2b04c18d54e250569f0a758cd6a4
Eero <eero.kahva@aalto.fi>
2019-08-05 22:26:11 +0300

    Return statements done

--- freeCodeCampTutorialProject/Program.cs
+++ freeCodeCampTutorialProject/Program.cs
@@ -10,12 +10,17 @@ namespace freeCodeCampTutorialProject
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("    /|");
-            Console.WriteLine("   / |");
-            Console.WriteLine("  /  |");
-            Console.WriteLine(" /___|");
+            int cubedNumber = Cube(5);
+            Console.WriteLine(cubedNumber);
 
             Console.ReadLine();
         }
+
+        static int Cube(int num)
+        {
+            int result = num * num * num;
+            return result;
+        }
+
     }
 }
