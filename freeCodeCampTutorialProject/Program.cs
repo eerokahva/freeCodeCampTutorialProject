6d9657cd6b9f44cc2823615977e8cc660b48ca37
Eero <eero.kahva@aalto.fi>
2019-08-06 20:03:56 +0300

    Better calculator done

--- freeCodeCampTutorialProject/Program.cs
+++ freeCodeCampTutorialProject/Program.cs
@@ -10,17 +10,37 @@ namespace freeCodeCampTutorialProject
     {
         static void Main(string[] args)
         {
-            int cubedNumber = Cube(5);
-            Console.WriteLine(cubedNumber);
+            Console.Write("Enter a number: ");
+            double num1 = Convert.ToDouble(Console.ReadLine());
+
+            Console.Write("Enter operator: ");
+            string op = Console.ReadLine();
+
+            Console.Write("Enter another number: ");
+            double num2 = Convert.ToDouble(Console.ReadLine());
+
+            if (op == "+")
+            {
+                Console.WriteLine(num1 + num2);
+            } else if (op == "-")
+            {
+                Console.WriteLine(num1 - num2);
+            }
+            else if (op == "*")
+            {
+                Console.WriteLine(num1 * num2);
+            }
+            else if (op == "/")
+            {
+                Console.WriteLine(num1 / num2);
+            } else
+            {
+                Console.WriteLine("Invalid operator!");
+            }
 
             Console.ReadLine();
         }
 
-        static int Cube(int num)
-        {
-            int result = num * num * num;
-            return result;
-        }
-
+        
     }
 }
